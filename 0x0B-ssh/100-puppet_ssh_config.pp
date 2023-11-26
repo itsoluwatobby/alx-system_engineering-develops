32f60d315ccbaf4bcd4c868974712ed9c3027703
itsoluwatobby <oluwasamuel050@gmail.com>
2023-11-26 19:20:31 +0100

    done

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# Using Puppet to setup ssh config file
+
+file_line { 'IdentityFile':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'PasswordAuthentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
