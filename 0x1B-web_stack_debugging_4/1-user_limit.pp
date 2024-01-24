a92b92efc0c2079206f4ba40737bc19aec79fdb9
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-01-24 09:37:07 +0100

    Create 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Setting up user holberton to login and open files without error.
+
+# hard file limit increase for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# soft file limit increase for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
