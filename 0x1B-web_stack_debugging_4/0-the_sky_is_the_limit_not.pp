36167f56d92beebbf2b340ef01dee27946b7a7ad
Oluwatobi Akinola Samuel <oluwasamuel050@gmail.com>
2024-01-24 09:35:04 +0100

    Create 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increases the amount of traffic handled by the Nginx server.
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Reload Nginx
+exec { 'nginx-reload':
+  command => 'nginx reload',
+  path    => '/etc/init.d/'
+}
